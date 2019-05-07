f61175e0a97042af3804c307592bd51983295675
roku674 <af03848@georgiasouthern.edu>
2019-05-06 20:37:51 -0400

    added reset info
    added reset infoi

---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity Algorithms
+
+If you see the Perilous Games Copyright don't worry about it it's my company I'm releasing certain code that could be helpful in any game
+-Alexander Fields
---
+++ Simple/ResetInfo.cs
@@ -0,0 +1,29 @@
+﻿//Copyright © 2017, Perilous Games, Ltd. All rights reserved.
+using UnityEngine;
+
+public class ResetInfo : MonoBehaviour
+{ 
+    public GameObject[] info = new GameObject[14];
+
+    /// <summary>
+    /// pass the number in to deactivate the certain GameObject
+    /// </summary>
+    /// <param name="infoNum"></param>
+    public void ChangeInfo(int infoNum)
+    {
+        InfoOff();
+        info[infoNum].SetActive(true);
+    }
+
+    public void InfoOff()
+    {
+        for (int i = 0; i < info.Length; i++)
+        {
+            if (info[i] != null)
+            {
+                info[i].SetActive(false);
+            }
+        }
+    }
+
+}
