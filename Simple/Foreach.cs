83aa8565f33c279400778f8d805ede596e647d15
roku674 <af03848@georgiasouthern.edu>
2019-08-25 13:02:02 -0400

    Update Foreach.cs

--- Simple/Foreach.cs
+++ Simple/Foreach.cs
@@ -1,7 +1,14 @@
 public class Foreach
 {
-  
-  public GameObject ByName(string name)
-  {
+    /// <summary>
+    /// Give it the name of the object you want to find and it will return it
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public GameObject ByName(string name)
+    {
+    }
+}
+
   
   
