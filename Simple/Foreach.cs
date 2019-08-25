370c7ef61a84a2dc71ac52911b1d6806ae20f1ec
roku674 <af03848@georgiasouthern.edu>
2019-08-25 13:12:25 -0400

    Update Foreach.cs

--- Simple/Foreach.cs
+++ Simple/Foreach.cs
@@ -1,12 +1,25 @@
-public class Foreach
+using UnityEngine;
+
+public class Foreach 
 {
     /// <summary>
-    /// Give it the name of the object you want to find and it will return it
+    /// Give it the name of the object you want to find and it will return it. If it returns null it was not found recommend you set up an if statement if (name != null)
     /// </summary>
-    /// <param name="name"></param>
+    /// <param name="name">Name of the object you desire</param>
+    /// <param name="rootGameObject">Parent gameobject it searches</param>
     /// <returns></returns>
-    public GameObject ByName(string name)
+    public GameObject ByName(string name, GameObject rootGameObject)
     {
+        GameObject children = GetComponentsInChildren<Transform>();
+        foreach (GameObject child in children)
+        {
+            if (child.name = name)
+            {
+                return child;
+            }
+        }
+
+        return child;
     }
 }
 
