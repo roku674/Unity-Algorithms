c3d5d0e2f48022d2f3202be917d16f0bfce63472
roku674 <af03848@georgiasouthern.edu>
2019-08-28 07:36:46 -0400

    Update Foreach.cs

--- Simple/Foreach.cs
+++ Simple/Foreach.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Foreach 
 {
     /// <summary>
-    /// Give it the name of the object you want to find and it will return it. If it returns null it was not found recommend you set up an if statement if (name != null)
+    /// Give it the name of the object you want to find and it will return all of them. If it returns null it was not found recommend you set up an if statement if (name != null)
     /// </summary>
     /// <param name="name">Name of the object you desire</param>
     /// <param name="rootGameObject">Parent gameobject it searches</param>
@@ -17,6 +17,30 @@ public class Foreach
         foreach (Transform child in children)
         {
             if (child.gameObject.name.Equals(name)) //if that doesn't work turn it into child.gameObject.name == name
+            {
+                childN[childCount] = child.gameObject; //assign to ChildN
+                childCount++;
+            }
+        }
+
+        return childN;
+    }
+
+    /// <summary>
+    /// Give it the tag of the object you want to find and it will return all of them. If it returns null it was not found recommend you set up an if statement if (name != null)
+    /// </summary>
+    /// <param name="name">Name of the object you desire</param>
+    /// <param name="rootGameObject">Parent gameobject it searches</param>
+    /// <returns></returns>
+    public GameObject[] ByTag(string tag, GameObject rootGameObject)
+    {
+        GameObject[] childN = null;
+
+        Transform[] children = rootGameObject.GetComponentsInChildren<Transform>();
+        int childCount = 0;
+        foreach (Transform child in children)
+        {
+            if (child.gameObject.name.tag == tag) //if that doesn't work turn it into child.gameObject.name == name
             {
                 childN[childCount] = child.gameObject; //assign to ChildN
                 childCount++;
